e31ee4ff7f57c81834f53ba25e006bcb6a9da53c
wsmeguro <masuday.55cm@wsmeguro.jp>
2019-04-27 16:14:47 +0900

    1st commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { MemberDetailComponent } from './member-detail.component';
+import { MembersComponent } from './members.component';
+import { MemberService } from './member.service';
+
+import {AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    MembersComponent,
+    MemberDetailComponent,
+  ],
+  bootstrap:    [
+    AppComponent
+  ],
+  providers: [ MemberService ]
+})
+export class AppModule { }
---
+++ src/app/member-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { Member } from './member';
+import { MemberService } from './member.service';
+
+@Component({
+  selector: 'member-detail', // <member-detail [member]="selectedMember"></member-detail>
+  templateUrl: './member-detail.component.html',
+  styleUrls:  ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  member: Member;
+
+  constructor(
+    private memberService: MemberService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap
+      .switchMap((param: ParamMap) => {
+        return this.memberService.getMember(+param.get('id'));
+      })
+      .subscribe(member => this.member = member);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
